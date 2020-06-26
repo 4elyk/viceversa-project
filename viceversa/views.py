6dc5682f2b2ce475cc2c34569b88f7db04ee7400
Bolatov Eldos <eldos__97@mail.ru>
2020-06-26 21:22:23 +0700

    viceversa_26.06_changes

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,5 +5,10 @@ def home(request):
 
 def reverse(request):
 	rev_text = request.GET['usertext']
+	words = 1
+	for i in range(0,len(rev_text)-1):
+		if rev_text[i] == " ":
+			words =words +1
+		print(words)
 	reversed_text = rev_text[::-1]
-	return render(request, 'reverse.html', {'usertext':rev_text, 'reversedtext':reversed_text})
+	return render(request, 'reverse.html', {'usertext':rev_text, 'reversedtext':reversed_text, 'sum_words':words})
